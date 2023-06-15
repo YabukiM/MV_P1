68b6b685c43367ae8b9cf08d2fff31a96799bb9b
Misael <misael.medinaventura25@gmail.com>
2023-06-15 10:40:13 -0700

    ug
    nj

--- inventQ.sql
+++ inventQ.sql
@@ -34,7 +34,6 @@ create table empleado(
 	nombre varchar (30),
 	seriePC varchar(50), 
 	puesto varchar(30),
-	cartacustiodia varchar(50) unique,
 	foreign key (seriePC) references Maquinas(seriePC)
 );
 ALTER TABLE empleado drop COLUMN cartacustiodia  
